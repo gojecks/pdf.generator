5998bd6978a7dad7bf13da1a0f0488f4bd7d49e6
Joseph Gojecks <gojeckskenwood@gmail.com>
2018-02-18 14:47:57 +0700

    added new implementation for defaultHelpers

---
+++ src/core/helpers.js
@@ -0,0 +1,60 @@
+pdfTemplateMaker.prototype.initializeDefaultHelpers = function() {
+    if (pdfTemplateMaker.$$defaultHelpers && typeof pdfTemplateMaker.$$defaultHelpers === 'object') {
+        for (var prop in pdfTemplateMaker.$$defaultHelpers) {
+            this.helpers.add(prop, pdfTemplateMaker.$$defaultHelpers[prop]);
+        }
+    }
+
+    return this;
+};
+
+pdfTemplateMaker.$$defaultHelpers = {
+    "@underline": parserFn({
+        decoration: "underline"
+    }),
+    "@bold": parserFn({
+        bold: true
+    }),
+    "@boldUnderline": parserFn({
+        decoration: "underline",
+        bold: true
+    }),
+    "@italics": parserFn({
+        italics: true
+    }),
+    "@boldItalics": parserFn({
+        italics: true,
+        bold: true
+    }),
+    "@boldItalicsUnderline": parserFn({
+        decoration: "underline",
+        bold: true,
+        italics: true
+    }),
+    "@underlineItalics": parserFn({
+        decoration: "underline",
+        italics: true
+    })
+};
+
+/**
+ * 
+ * @param {*} definition 
+ */
+function parserFn(definition) {
+    return function(parser) {
+        var nArr = new Array(parser.length);
+        parser.input().forEach(function(str, idx) {
+            if (parser.inArray(str)) {
+                var _obj = JSON.parse(JSON.stringify(definition));
+                _obj.text = str;
+                str = _obj;
+                _obj = null;
+            }
+
+            nArr[idx] = str
+        });
+
+        return nArr;
+    }
+}
